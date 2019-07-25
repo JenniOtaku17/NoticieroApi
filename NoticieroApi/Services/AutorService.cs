063705bfc54784fd51472b03c00b7f5ecc90a4e7
Jennifer <20175329@itla.edu.do>
2019-07-25 12:03:29 -0800

    Servicios agregados

---
+++ NoticieroApi/Services/AutorService.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoticieroApi.Models;
+using NoticieroApi.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoticieroApi.Services
+{
+    public class AutorService
+    {
+        public readonly NoticieroDbContext _AutorDB;
+        public AutorService(NoticieroDbContext AutorDB)
+        {
+            _AutorDB = AutorDB;
+        }
+
+        public List<Autor> VerAutor()
+        {
+            var AutorBuscado = _AutorDB.Autor.OrderByDescending(x => x.IdAutor).ToList();
+            return AutorBuscado;
+        }
+
+        public bool Agregar(Autor AutorAgregar)
+        {
+            try
+            {
+                _AutorDB.Autor.Add(AutorAgregar);
+                _AutorDB.SaveChanges();
+                return true;
+
+            }
+            catch (Exception error)
+            {
+                return false;
+
+            }
+
+        }
+
+        public bool Editar(Autor AutorEditar)
+        {
+            try
+            {
+                var Autor = _AutorDB.Autor.FirstOrDefault(x => x.IdAutor ==AutorEditar.IdAutor);
+                Autor.nombre = AutorEditar.nombre;
+                Autor.apellido = AutorEditar.apellido;
+                _AutorDB.SaveChanges();
+
+                return true;
+
+            }
+            catch (Exception error)
+            {
+                return false;
+            }
+        }
+
+        public bool Eliminar(int IdAutor)
+        {
+            try
+            {
+                var AutorEliminar = _AutorDB.Autor.FirstOrDefault(x => x.IdAutor == IdAutor);
+                _AutorDB.Autor.Remove(AutorEliminar);
+                _AutorDB.SaveChanges();
+
+                return true;
+
+            }
+            catch (Exception error)
+            {
+                return false;
+            }
+
+        }
+
+     
+    }
+}
---
+++ NoticieroApi/Services/UsuarioService.cs
@@ -0,0 +1,78 @@
+﻿using NoticieroApi.Models;
+using NoticieroApi.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoticieroApi.Services
+{
+    public class UsuarioService
+    {
+        public readonly NoticieroDbContext _UsuarioDB;
+        public UsuarioService(NoticieroDbContext UsuarioDB)
+        {
+            _UsuarioDB = UsuarioDB;
+        }
+
+        public List<Usuario> VerUsuario()
+        {
+            var UsuarioBuscado = _UsuarioDB.Usuario.OrderByDescending(x => x.IdUsuario).ToList();
+            return UsuarioBuscado;
+        }
+
+        public bool Agregar(Usuario UsuarioAgregar)
+        {
+            try
+            {
+                _UsuarioDB.Usuario.Add(UsuarioAgregar);
+                _UsuarioDB.SaveChanges();
+                return true;
+
+            }
+            catch (Exception error)
+            {
+                return false;
+
+            }
+
+        }
+
+        public bool Editar(Usuario UsuarioEditar)
+        {
+            try
+            {
+                var Usuario = _UsuarioDB.Usuario.FirstOrDefault(x => x.IdUsuario == UsuarioEditar.IdUsuario);
+                Usuario.alias = UsuarioEditar.alias;
+                Usuario.email = UsuarioEditar.email;
+                Usuario.contrasenia = UsuarioEditar.contrasenia;
+                _UsuarioDB.SaveChanges();
+
+                return true;
+
+            }
+            catch (Exception error)
+            {
+                return false;
+            }
+        }
+
+        public bool Eliminar(int IdUsuario)
+        {
+            try
+            {
+                var UsuarioEliminar = _UsuarioDB.Usuario.FirstOrDefault(x => x.IdUsuario == IdUsuario);
+                _UsuarioDB.Usuario.Remove(UsuarioEliminar);
+                _UsuarioDB.SaveChanges();
+
+                return true;
+
+            }
+            catch (Exception error)
+            {
+                return false;
+            }
+
+        }
+    }
+}
