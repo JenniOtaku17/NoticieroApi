40b8de80a6913423e65a2f720ddd360dceec667c
Jennifer <20175329@itla.edu.do>
2019-08-06 15:05:10 -0800

    Agregando pruebas unitarias

---
+++ NoticieroApiTest/NoticiaTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NoticieroApi.Models;
+
+namespace NoticieroApiTest
+{
+    [TestClass]
+    class NoticiaTests
+    {
+        [TestMethod]
+        public void NoestaVacio()
+        {
+            var Noticia = new Noticia();
+            var result = Noticia.ToString();
+            Assert.IsNotNull(result);
+
+        }
+    }
+}
---
+++ NoticieroApiTest/UsuarioTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NoticieroApi.Models;
+
+namespace NoticieroApiTest
+{
+    [TestClass]
+    class UsuarioTests
+    {
+        [TestMethod]
+        public void NoestaVacio()
+        {
+            var Usuario = new Usuario();
+            var result = Usuario.ToString();
+            Assert.IsNotNull(result);
+
+        }
+    }
+}
